a27caceff8f9a8c37da63fd66d0443fad9d25611
Martin Krocil <mart1n.krocil@seznam.cz>
2023-02-04 09:48:18 +0100

    substr first attempt

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mkrocil <mkrocil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 09:24:16 by mkrocil           #+#    #+#             */
-/*   Updated: 2023/02/04 09:34:58 by mkrocil          ###   ########.fr       */
+/*   Updated: 2023/02/04 09:47:32 by mkrocil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	
+	unsigned int	i;
+	unsigned int	j;
+	char			*substr;
+
+	i = 0;
+	j = 0;
+	substr = malloc(sizeof(char) * len);
+	while(s[i])
+	{
+		while(i == start && len >= j)
+		{
+			substr[j] = s[i + j];
+			j++;
+		}
+		i++;
+	}
+	return (substr);
 }
